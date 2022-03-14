9df2397c5ef6e50e28e6b12216614e7f6356e447
yskuridov <yskuridov@gmail.com>
2022-03-13 23:40:52 -0400

    DVD model implemented

--- src/main/java/ca/cal/tp/model/Document/Dvd.java
+++ src/main/java/ca/cal/tp/model/Document/Dvd.java
@@ -1,4 +1,25 @@
 package ca.cal.tp.model.Document;
 
-public class Dvd {
+import ca.cal.tp.model.Document.Abstract.Document;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Dvd extends Document {
+    private int durationInMinutes;
+    private String category;
+    private String mainActor;
+    private static final int DVD_LOAN_LENGTH = 7; //days
+
+    public Dvd(int id, String title, int publicationYear, String language, int durationInMinutes, String category, String mainActor) {
+        super(id, title, publicationYear, language);
+        this.durationInMinutes = durationInMinutes;
+        this.category = category;
+        this.mainActor = mainActor;
+        this.setLoanLength(DVD_LOAN_LENGTH);
+    }
 }
