cf5067fa0f7d8b402dd83b50e0422ea87761258e
yskuridov <yskuridov@gmail.com>
2022-03-13 23:38:42 -0400

    CD model implemented

--- src/main/java/ca/cal/tp/model/Document/Cd.java
+++ src/main/java/ca/cal/tp/model/Document/Cd.java
@@ -1,4 +1,25 @@
 package ca.cal.tp.model.Document;
 
-public class Cd {
+import ca.cal.tp.model.Document.Abstract.Document;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Cd extends Document {
+    private String singer;
+    private int numberOfTracks;
+    private String genre;
+    private static final int CD_LOAN_LENGTH = 14; //days
+
+    public Cd(int id, String title, int publicationYear, String language, String singer, int numberOfTracks, String genre) {
+        super(id, title, publicationYear, language);
+        this.singer = singer;
+        this.numberOfTracks = numberOfTracks;
+        this.genre = genre;
+        this.setLoanLength(CD_LOAN_LENGTH);
+    }
 }
