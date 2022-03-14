4e22d3b2bc24ecd642140cf2b6cb57e296c4375a
yskuridov <yskuridov@gmail.com>
2022-03-13 23:34:31 -0400

    Book model implemented

--- src/main/java/ca/cal/tp/model/Document/Book.java
+++ src/main/java/ca/cal/tp/model/Document/Book.java
@@ -1,7 +1,18 @@
 package ca.cal.tp.model.Document;
 
 import ca.cal.tp.model.Document.Abstract.Document;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
 public class Book extends Document {
-
+    private String author;
+    private String editor;
+    private int pageAmount;
+    private String genre;
+    private static final int BOOK_LOAN_LENGTH = 21; //days
 }
